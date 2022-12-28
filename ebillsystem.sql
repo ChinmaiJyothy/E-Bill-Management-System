8656c016dfb9dcd4a762af55c171e84f718febeb
ChinmaiJyothy <97036679+ChinmaiJyothy@users.noreply.github.com>
2022-12-28 18:44:17 +0530

    Add files via upload

---
+++ ebillsystem.sql
@@ -0,0 +1,311 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 28, 2022 at 03:21 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ebillsystem`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `unitstoamount` (IN `units` INT(14), OUT `result` INT(14))  BEGIN
+   
+    DECLARE a INT(14) DEFAULT 0;
+    DECLARE b INT(14) DEFAULT 0;
+    DECLARE d INT(14) DEFAULT 0;
+
+    SELECT twohundred FROM unitsRate INTO a ;
+    SELECT fivehundred FROM unitsRate INTO b ;
+    SELECT thousand FROM unitsRate INTO d  ;
+
+    IF units<200
+    then
+        SELECT a*units INTO result;
+    
+    ELSEIF units<500
+    then
+        SELECT (a*200)+(b*(units-200)) INTO result;
+    ELSEIF units > 500
+    then
+        SELECT (a*200)+(b*(300))+(d*(units-500)) INTO result;
+    END IF;
+
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `curdate1` () RETURNS INT(11) BEGIN
+    DECLARE x INT;
+    SET x = DAYOFMONTH(CURDATE());
+    IF (x=1)
+    THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(14) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `pass` varchar(21) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `email`, `pass`) VALUES
+(1, 'Chinmai Jyothy', 'admin@gmail.com', 'admin123'),
+(2, 'Spoorthi A', 'admin2@gmail.com', 'admin456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(14) NOT NULL,
+  `aid` int(14) NOT NULL,
+  `uid` int(14) NOT NULL,
+  `units` int(10) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `bdate` date NOT NULL,
+  `ddate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id`, `aid`, `uid`, `units`, `amount`, `status`, `bdate`, `ddate`) VALUES
+(27, 1, 12, 100, '200.00', 'PROCESSED', '2022-01-28', '2022-02-27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `id` int(14) NOT NULL,
+  `uid` int(14) NOT NULL,
+  `aid` int(14) NOT NULL,
+  `complaint` varchar(140) NOT NULL,
+  `status` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`id`, `uid`, `aid`, `complaint`, `status`) VALUES
+(14, 12, 2, 'Bill Generated Late', 'PROCESSED');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(14) NOT NULL,
+  `bid` int(14) NOT NULL,
+  `payable` decimal(10,2) NOT NULL,
+  `pdate` date DEFAULT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`id`, `bid`, `payable`, `pdate`, `status`) VALUES
+(27, 27, '200.00', '2022-01-28', 'PROCESSED');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unitsRate`
+--
+
+CREATE TABLE `unitsRate` (
+  `sno` int(1) DEFAULT NULL,
+  `twohundred` int(14) NOT NULL,
+  `fivehundred` int(14) NOT NULL,
+  `thousand` int(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unitsRate`
+--
+
+INSERT INTO `unitsRate` (`sno`, `twohundred`, `fivehundred`, `thousand`) VALUES
+(1, 2, 5, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(14) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `pass` varchar(21) NOT NULL,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `phone`, `pass`, `address`) VALUES
+(12, 'chinmai jyothy', 'asdf@gmail.com', '9876543210', 'asdf123', 'bangalore'),
+(13, 'spoorthi', 'spoorthi@gmail.com', '9876543210', 'spoorthi123', 'mysore'),
+(14, 'JAYALAKSHMI', 'jaya@gmail.com', '987654301', 'jaya123', 'andraPradesh');
+
+--
+-- Triggers `user`
+--
+DELIMITER $$
+CREATE TRIGGER `BEF_INSERT` BEFORE INSERT ON `user` FOR EACH ROW BEGIN
+
+SET NEW.NAME=UPPER(NEW.NAME);
+
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `aid` (`aid`),
+  ADD KEY `uid` (`uid`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `aid` (`aid`),
+  ADD KEY `uid` (`uid`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bid` (`bid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`aid`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bill_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD CONSTRAINT `complaint_ibfk_1` FOREIGN KEY (`aid`) REFERENCES `admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `complaint_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`bid`) REFERENCES `bill` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
